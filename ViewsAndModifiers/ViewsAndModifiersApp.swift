76a603f00bb578eab0e477a999b88db7f871af94
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-24 11:32:28 +0300

    Initial commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by jazeps.ivulis on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
