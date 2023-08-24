9a66c0fa921a3a43fd639370cdf8056978fe04b5
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-24 16:32:25 +0300

    Implemented examples from Day 23 lessons

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,153 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.title)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var useRedText = false
+    
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    
+    let motto2 = Text("nunquam titillandus")
+    
+    var spells: some View {
+        Group {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    
+    @ViewBuilder var greetings: some View {
+        Text("Good")
+        Text("Morning")
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.blue)
+                .watermarked(with: "This is a watermark")
+            
+            Button("Tap me") {
+                print(type(of: self.body))
+                useRedText.toggle()
+            }
+            .foregroundColor(useRedText ? .red : .blue)
+            
+            VStack {
+                Text("Gryffindor")
+                    .font(.largeTitle)
+                Text("Hufflepuff")
+                Text("Ravenclaw")
+                Text("Slytherin")
+            }
+            .font(.title)
+            
+            VStack {
+                spells
+            }
+            
+            HStack {
+                spells
+            }
+            
+            VStack {
+                greetings
+            }
+            
+            HStack {
+                greetings
+            }
+            
+            VStack(spacing: 10) {
+                CapsuleText(text: "First")
+                    .foregroundColor(.white)
+                CapsuleText(text: "Second")
+                    .foregroundColor(.yellow)
+            }
+            
+            Text("Hello")
+                .titleStyle()
+        }
+        
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
     }
 }
 
